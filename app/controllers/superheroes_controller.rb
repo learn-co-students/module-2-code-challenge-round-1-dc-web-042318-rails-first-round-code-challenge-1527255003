315f321ae11ec9e9a1ab7232b5d4c3c7d76ea2ae
codingkasia <codingkasia@gmail.com>
2018-05-25 10:47:08 -0400

    done

--- app/controllers/superheroes_controller.rb
+++ app/controllers/superheroes_controller.rb
@@ -2,4 +2,34 @@ class SuperheroesController < ApplicationController
   def index
     @superheroes = Superhero.all
   end
+
+  def new
+    @superhero = Superhero.new
+  end
+  def create
+
+    @superhero = Superhero.create(superhero_params)
+    redirect_to @superhero
+
+    #with validation: 
+    # @superhero = Superhero.new(superhero_params)
+    # if @superhero.valid?
+    #   @superhero.save
+    # else
+    #   render 'new'
+    # end
+  end
+
+  def show
+    @superhero = Superhero.find(params[:id])
+  end
+  
+
+  private
+
+  def superhero_params
+    params.require(:superhero).permit(:name, :super_name, :superpower_id, :superpower_id => [])
+
+  end
+
 end
--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,24 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  # def new
+  #   @superpower = Superpower.new
+  # end
+  # def create
+  #   @superpower = Superpower.create(superpower_params)
+  # end
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+  
+
+  private 
+
+  def superpower_params
+    params.require(:superpower).permit(:name, :description)
+
+  end
+
+
 end
