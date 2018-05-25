963b43c3a0ef10ccad0ea495808669857228a5aa
github username <github email address>
2018-05-25 10:46:14 -0400

    done

--- app/controllers/superpowers_controller.rb
+++ app/controllers/superpowers_controller.rb
@@ -2,4 +2,29 @@ class SuperpowersController < ApplicationController
   def index
     @superpowers = Superpower.all
   end
+
+  def new
+    @superpower = Superpower.new
+  end
+
+  def create
+    @superpower = Superpower.new(superpower_params)
+    @superpower.save
+    if superpower.valid?
+      redirect_to superpower_path(@superhero)
+    else
+      render 'new'
+    end
+  end
+
+  def show
+    @superpower = Superpower.find(params[:id])
+  end
+
+
+  private
+
+  def superpower_params
+    params.require(:superpower).permit(:name, :description)
+  end
 end
